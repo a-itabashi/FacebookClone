f269f0630bb9f80171e4505a042ea2464b759ea4
Aguru itabashi <itabashi.a@gmail.com>
2019-03-18 17:59:41 +0900

    blogs_controllerをgenerate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+
+  get 'blogs/new'
+
+  get 'blogs/edit'
+
+  get 'blogs/confirm'
+
   root to: 'tops#index'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
---
+++ db/migrate/20190318085846_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :content
+      t.text :image
+      t.string :user_references
+
+      t.timestamps
+    end
+  end
+end
