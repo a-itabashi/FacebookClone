b32f069a15e7b7e036581288772be5bf5623d067
Aguru itabashi <itabashi.a@gmail.com>
2019-03-19 01:03:57 +0900

    applocation.jsファイルを修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 
-
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap.min
 //= require rails-ujs
 //= require jquery/dist/jquery.js
