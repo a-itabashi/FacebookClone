520402c61b930121fedd190b7cf0fc6c9f74e562
Aguru itabashi <itabashi.a@gmail.com>
2019-03-18 17:44:52 +0900

    sessions_controllerのアクション設定完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      session[user_id] = @user.id
+      flash[:success] = "ログインに成功しました"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:info] = "ログアウトに成功しました"
+    redirect_to root_path
+  end
 end
