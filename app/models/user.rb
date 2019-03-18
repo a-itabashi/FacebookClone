5058a5bf046830c6888174068f13724ae8f41f47
Aguru itabashi <itabashi.a@gmail.com>
2019-03-18 16:44:09 +0900

    userモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum:20}
+  
+  validates :email, presence: true, length: {maximum:50},
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true
+  before_validation {email.downcase!}
+
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 5}
 end
