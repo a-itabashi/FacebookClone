9d205c99db282146945b2efa922b8906ef593ca4
Aguru itabashi <itabashi.a@gmail.com>
2019-03-18 17:33:32 +0900

    ユーザー登録のコントローラー設定完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -7,18 +9,40 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, flash[:success] = "ユーザー登録が完了しました"
+      flash[:success] = 'ユーザー登録が完了しました'
+      redirect_to root_path
     else
       render 'new'
     end
   end
 
+  def show
+  end
+
   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'プロフィールを更新しました'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:info] = "ユーザー登録情報を消去しました"
+    redirect_to root_path 
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+   @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # topページ
   root to: 'tops#index'
-
   resources :users
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
 end
